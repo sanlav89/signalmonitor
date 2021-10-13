54441c15fd0f3b586768addfed9e451f327011c8
sanlav89 <sanlav89@mail.ru>
2021-10-13 17:34:32 +0300

    Первый график на форме

--- signalmonitor.cpp
+++ signalmonitor.cpp
@@ -1,8 +1,20 @@
 #include "signalmonitor.h"
+#include <QGridLayout>
 
 SignalMonitor::SignalMonitor(QWidget *parent)
     : QWidget(parent)
 {
+    m_graph = new GraphBase(this);
+    QGridLayout *mainLayout = new QGridLayout(this);
+    mainLayout->addWidget(m_graph);
+    setLayout(mainLayout);
+
+    int n = 100;
+    QVector<QPointF> testData(n);
+    for (int i = 0; i < n; i++) {
+        testData[i] = QPointF(i, rand());
+    }
+    m_graph->setValues(testData);
 }
 
 SignalMonitor::~SignalMonitor()
--- signalmonitor.h
+++ signalmonitor.h
@@ -2,6 +2,7 @@
 #define SIGNALMONITOR_H
 
 #include <QWidget>
+#include "graphbase.h"
 
 class SignalMonitor : public QWidget
 {
@@ -10,5 +11,8 @@ class SignalMonitor : public QWidget
 public:
     SignalMonitor(QWidget *parent = nullptr);
     ~SignalMonitor();
+
+    GraphBase *m_graph;
+
 };
 #endif // SIGNALMONITOR_H
